ab402f9997d75f99b0db4239e2178bef1a0dd2a9
AspirinaSenpai <89920438+AspirinaSenpai@users.noreply.github.com>
2025-07-20 13:50:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -311,3 +311,7 @@ def limpar_campos():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
